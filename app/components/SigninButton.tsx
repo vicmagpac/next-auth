0f6069a037c66a2c3c1e40fca0045e7fb1f19329
Victor Magalhaes <victor.magalhaesp@gmail.com>
2023-12-03 14:46:33 -0300

    login and logout google

--- app/components/SigninButton.tsx
+++ app/components/SigninButton.tsx
@@ -1,24 +1,39 @@
-"use client"
+"use client";
 
 import React from "react";
 
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const SigninButton = () => {
+  const { data: session } = useSession();
+  console.log(session);
+
   return (
     <>
-      <button
-        type="button"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onClick={() => signIn("google")}
-      >
-        Login
-      </button>
-      <img
-        className="w-10 h-10 rounded-full"
-        src="/DeveloperDeck101.png"
-        alt="Rounded avatar"
-      ></img>
+      {session ? (
+        <div>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => signOut()}
+          >
+            Sair
+          </button>
+          <span>
+            {session.user?.name}
+          </span>
+        </div>
+      ) : (
+        <div>
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            onClick={() => signIn("google")}
+          >
+            Login
+          </button>
+        </div>
+      )}
     </>
   );
 };
